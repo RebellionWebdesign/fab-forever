bf5fbd476aeddea20bd4ce88ca94826bfca09d44
Daniel Stauffer <info@rebellion-webdesign.com>
2023-07-07 11:49:33 +0200

    Add removal of data in local storage

--- assets/js/show.js
+++ assets/js/show.js
@@ -2,5 +2,6 @@ function show() {
     if (localStorage.getItem("isChecked") = "yes") {
        const showText = document.getElementById("hidden")
        showText.classList.remove("hide")
+       localStorage.removeItem("isChecked")
     }
 }
