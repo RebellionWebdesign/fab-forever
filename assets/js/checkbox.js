c0ad612696b8aea14b4df6db78a5e6ef98f99f24
Daniel Stauffer <info@rebellion-webdesign.com>
2023-07-09 13:19:07 +0200

    Delete js scripts

--- assets/js/checkbox.js
+++
@@ -1,5 +0,0 @@
-function checkbox() {
-    if (document.getElementsByClassName("newsletter").checked) {
-        localStorage.setItem("isChecked", "yes")
-    }
-}
--- assets/js/show.js
+++
@@ -1,7 +0,0 @@
-function show() {
-    if (localStorage.getItem("isChecked") = "yes") {
-       const showText = document.getElementById("hidden")
-       showText.classList.remove("hide")
-       localStorage.removeItem("isChecked")
-    }
-}
